8dec6f499ec267f1cdc52f380fd3b4eaa19b1854
letronghuyit <letronghuyit@gmail.com>
2025-04-13 16:42:56 +0700

    bug install

--- BE/admin/vendor/composer/installed.php
+++ BE/admin/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => 'fed37a565826c943b49d7244e5486436be357986',
+        'reference' => 'da1fff0aef975e8fb88d1bad84f7bf6c31831b2b',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -409,7 +409,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => 'fed37a565826c943b49d7244e5486436be357986',
+            'reference' => 'da1fff0aef975e8fb88d1bad84f7bf6c31831b2b',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
