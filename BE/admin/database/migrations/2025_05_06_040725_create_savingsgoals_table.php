2997f2c67080bc3b92226d7351464a7b36dfa2f2
letronghuyit <letronghuyit@gmail.com>
2025-05-06 13:49:55 +0700

    feat/feat-check-role-and-saving-goals

---
+++ BE/admin/database/migrations/2025_05_06_040725_create_savingsgoals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('savingsgoals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->double('target');
+            $table->double('save_money');
+            $table->date('start_day');
+            $table->date('end_day');
+            $table->double('savings_percentage');
+            $table->double('save_money_today')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('savingsgoals');
+    }
+};
