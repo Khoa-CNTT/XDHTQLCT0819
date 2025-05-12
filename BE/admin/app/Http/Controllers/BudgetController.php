2aa6d639bf54d0787992161d7887258860dd586c
letronghuyit <letronghuyit@gmail.com>
2025-05-12 22:17:36 +0700

    bug/bug-res/budget

--- BE/admin/app/Http/Controllers/BudgetController.php
+++ BE/admin/app/Http/Controllers/BudgetController.php
@@ -150,7 +150,7 @@ class BudgetController extends Controller
         }
 
         return response()->json([
-            'canh_bao' => $alerts
+            'alerts' => $alerts
         ]);
     }
 
